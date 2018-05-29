318775bfda468841c35e4c22baed078296f254fa
karak <karak97@gmail.com>
2018-05-29 22:56:09 +0900

    プロジェクト ファイルを追加します。

---
+++ XF3Eval/XF3Eval/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using XF3Eval.Models;
+
+namespace XF3Eval.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
